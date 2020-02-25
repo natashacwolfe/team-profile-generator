5dd44d1e8d47757488703e6a27c483341cb2b6e4
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-02-24 17:59:21 -0800

    updated formating and added screenshot

--- app.js
+++ app.js
@@ -64,122 +64,122 @@ let internPrompts = [
     }
 ];
 
-function init(){
+function init() {
     inquirer
         .prompt(prompts)
         .then(empRes => {
 
-        if (empRes.role === 'Manager') {
-            inquirer
-                .prompt(managerPrompts)
-                .then (manRes => {
-                    let manager = new Manager (empRes.name, empRes.id, empRes.email, empRes.role, manRes.officeNumber);
-                    employeesArray.push(manager);
-                    console.log("Employee Array", employeesArray)
+            if (empRes.role === 'Manager') {
+                inquirer
+                    .prompt(managerPrompts)
+                    .then(manRes => {
+                        let manager = new Manager(empRes.name, empRes.id, empRes.email, empRes.role, manRes.officeNumber);
+                        employeesArray.push(manager);
+                        console.log("Employee Array", employeesArray)
 
-                    nextEmployee();
-            }) 
-        } 
-        else if (empRes.role === 'Engineer') {
-            inquirer
-                .prompt(engineerPrompts)
-                .then(engRes => {
-                    let engineer = new Engineer (empRes.name, empRes.id, empRes.email, empRes.role, engRes.github);
-                    
-                employeesArray.push(engineer);
-                console.log("Employee Array", employeesArray)
+                        nextEmployee();
+                    })
+            }
+            else if (empRes.role === 'Engineer') {
+                inquirer
+                    .prompt(engineerPrompts)
+                    .then(engRes => {
+                        let engineer = new Engineer(empRes.name, empRes.id, empRes.email, empRes.role, engRes.github);
 
-                nextEmployee();
-            })
-        }
-        else if (empRes.role === "Intern") {
-            inquirer
-                .prompt(internPrompts)
-                .then(intRes => {
-                    let intern = new Intern (empRes.name, empRes.id, empRes.email, empRes.role, intRes.school);
+                        employeesArray.push(engineer);
+                        console.log("Employee Array", employeesArray)
 
-                    employeesArray.push(intern);
-                    console.log("Employee Array", employeesArray)
+                        nextEmployee();
+                    })
+            }
+            else if (empRes.role === "Intern") {
+                inquirer
+                    .prompt(internPrompts)
+                    .then(intRes => {
+                        let intern = new Intern(empRes.name, empRes.id, empRes.email, empRes.role, intRes.school);
 
-                    nextEmployee();
-            })
-        }
-    }) 
-    .catch ((err) => {
-        if (err) console.log(err);
-    });
+                        employeesArray.push(intern);
+                        console.log("Employee Array", employeesArray)
+
+                        nextEmployee();
+                    })
+            }
+        })
+        .catch((err) => {
+            if (err) console.log(err);
+        });
 };
 
-function nextEmployee(){
+function nextEmployee() {
     inquirer
         .prompt([
             {
                 "type": "list",
                 "message": "Do you want to add another employee?",
                 "choices": ["Add employee", 'Exit application'],
-                "name" : "option"
+                "name": "option"
             }
         ])
-        .then (res => {
-            switch (res.option){
-                case "Add employee" : init()
-                break;
-                case "Exit application" : renderHtml()
-                process.exit(0)
+        .then(res => {
+            switch (res.option) {
+                case "Add employee": init()
+                    break;
+                case "Exit application": renderHtml()
+                    process.exit(0)
             }
         })
-        .catch ((err) => {
+        .catch((err) => {
             if (err) console.log(err);
         });
 };
 
 
 function renderHtml() {
-    
+
     let htmlContent = ""
-    htmlContent = indexTemplate.renderHeader(); 
+    htmlContent = indexTemplate.renderHeader();
     for (let i = 0; i < employeesArray.length; i++) {
         switch (employeesArray[i].role) {
-            case "Manager" : let managerObj = {
-                name : employeesArray[i].getName(),
-                id : employeesArray[i].getId(),
-                email : employeesArray[i].getEmail(),
+            case "Manager": let managerObj = {
+                name: employeesArray[i].getName(),
+                id: employeesArray[i].getId(),
+                email: employeesArray[i].getEmail(),
                 role: employeesArray[i].getRole(),
-                officeNumber : employeesArray[i].getOfficeNumber()
-            } 
-            console.log(managerObj)
-            htmlContent += managerTemplate(managerObj);
-            break;
+                officeNumber: employeesArray[i].getOfficeNumber()
+            }
+                console.log(managerObj)
+                htmlContent += managerTemplate(managerObj);
+                break;
 
-            case "Engineer" : let engineerObj = {
-                name : employeesArray[i].getName(),
-                id : employeesArray[i].getId(),
-                email : employeesArray[i].getEmail(),
+            case "Engineer": let engineerObj = {
+                name: employeesArray[i].getName(),
+                id: employeesArray[i].getId(),
+                email: employeesArray[i].getEmail(),
                 role: employeesArray[i].getRole(),
-                github : employeesArray[i].getGithub()
+                github: employeesArray[i].getGithub()
             }
 
-            htmlContent += engineerTemplate(engineerObj);
-            break;
+                htmlContent += engineerTemplate(engineerObj);
+                break;
 
-            case "Intern" : let internObj = {
-                name : employeesArray[i].getName(),
-                id : employeesArray[i].getId(),
-                email : employeesArray[i].getEmail(),
+            case "Intern": let internObj = {
+                name: employeesArray[i].getName(),
+                id: employeesArray[i].getId(),
+                email: employeesArray[i].getEmail(),
                 role: employeesArray[i].getRole(),
-                school : employeesArray[i].getSchool()
+                school: employeesArray[i].getSchool()
             }
 
-            htmlContent += internTemplate(internObj);
-            break;
+                htmlContent += internTemplate(internObj);
+                break;
         }
     }
     console.log(htmlContent)
     htmlContent += indexTemplate.renderFooter();
 
     fs.writeFileSync(outputPath, htmlContent, "utf-8");
-        console.log("File updated!", htmlContent);
-    
+    console.log("File updated!", htmlContent);
+
 };
 
 init();
